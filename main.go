47c9c09b351067611efdd7f51d74fd689bacb9d2
Alonso Codesal <acodem00@estudiantes.unileon.es>
2025-03-06 17:17:38 +0100

    Fixing

--- main.go
+++ main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
 	_ "github.com/google/uuid" // Dependencia temporal
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello world!")
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	// Configuramos la ruta raíz para que use la función helloHandler
+	http.HandleFunc("/", helloHandler)
+
+	// Definimos el puerto en el que se ejecutará el servidor
+	port := "8080"
+	log.Printf("Servidor web iniciado en el puerto %s", port)
+
+	// Iniciamos el servidor y mostramos un error si ocurre
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("Error al iniciar el servidor: %v", err)
+	}
 }
