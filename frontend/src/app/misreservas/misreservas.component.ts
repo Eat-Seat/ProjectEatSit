cc5b146b1ab641e1a42b1ebb817198089ef5697c
Luis Laballos <llabag01@estudiantes.unileon.es>
2025-05-11 21:46:52 +0200

    Update and Deleted bookinbg is working

--- frontend/src/app/misreservas/misreservas.component.ts
+++ frontend/src/app/misreservas/misreservas.component.ts
@@ -12,6 +12,16 @@ import { CommonModule } from '@angular/common';
 export class MisreservasComponent implements OnInit {
   reservas: any[] = [];
   user: any = null;
+  editandoId: number | null = null;
+  formularioEdicion = {
+    nombre: '',
+    apellidos: '',
+    telefono: '',
+    personas: 1,
+    fecha: '',
+    hora: ''
+  };
+
 
   constructor(private authService: AuthService) {}
 
@@ -44,5 +54,52 @@ export class MisreservasComponent implements OnInit {
       return acc;
     }, {} as { [key: string]: any[] });
   }
+  borrarReserva(reservaId: number): void {
+  if (confirm('¿Estás seguro de que quieres borrar esta reserva?')) {
+    fetch(`http://localhost:3000/reservas/${reservaId}`, {
+      method: 'DELETE'
+    })
+      .then(res => res.json())
+      .then(() => {
+        this.reservas = this.reservas.filter(r => r.reserva_id !== reservaId);
+      })
+      .catch(error => {
+        console.error('Error al borrar la reserva:', error);
+      });
+  }
+}
+  editarReserva(r: any) {
+    this.editandoId = r.reserva_id;
+    this.formularioEdicion = {
+      nombre: r.nombre,
+      apellidos: r.apellidos,
+      telefono: r.telefono,
+      personas: r.personas,
+      fecha: r.fecha,
+      hora: r.hora
+    };
+  }
+  guardarEdicion(r: any) {
+  const id = r.reserva_id;
+
+  fetch(`http://localhost:3000/reservas/${id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(this.formularioEdicion)
+  })
+    .then(res => res.json())
+    .then(data => {
+      if (data.success) {
+        Object.assign(r, this.formularioEdicion);
+        this.editandoId = null;
+      } else {
+        alert('Error al actualizar la reserva');
+      }
+    })
+    .catch(error => {
+      console.error('Error al guardar la edición:', error);
+      alert('Error de conexión con el servidor');
+    });
+}
 }
 
