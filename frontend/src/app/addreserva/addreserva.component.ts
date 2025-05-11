c323dd5b21476a780d11649e91848f02597d43d0
Luis Laballos <llabag01@estudiantes.unileon.es>
2025-05-11 19:58:33 +0200

    Frontend of add booking made it

--- frontend/src/app/addreserva/addreserva.component.ts
+++ frontend/src/app/addreserva/addreserva.component.ts
@@ -1,11 +1,74 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-addreserva',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './addreserva.component.html',
   styleUrl: './addreserva.component.css'
 })
-export class AddreservaComponent {
+export class AddreservaComponent implements OnInit{
+  
+  groupedRestaurantes: { [ciudad: string]: any[] } = {};
+  formularioAbiertoId: number | null = null;
 
+reservaForm = {
+  nombre: '',
+  apellidos: '',
+  telefono: '',
+  personas: null,
+  fecha: ''
+};
+
+  ngOnInit() {
+    fetch('http://localhost:3000/restaurants')
+      .then(res => res.json())
+      .then(data => {
+        this.groupedRestaurantes = this.groupByCity(data);
+      });
+  }
+
+  groupByCity(data: any[]): { [ciudad: string]: any[] } {
+    return data.reduce((acc, curr) => {
+      const ciudad = curr.ciudad || 'Unknown';
+      if (!acc[ciudad]) {
+        acc[ciudad] = [];
+      }
+      acc[ciudad].push(curr);
+      return acc;
+    }, {} as { [key: string]: any[] });
+  }
+  addreserva(r: any) {
+    this.formularioAbiertoId = r.id;
+    this.reservaForm = {
+      nombre: '',
+      apellidos: '',
+      telefono: '',
+      personas: null,
+      fecha: ''
+    };
+  }
+  guardarReserva(restauranteId: number) {
+  const body = {
+    ...this.reservaForm,
+    restaurante_id: restauranteId
+  };
+
+  fetch('http://localhost:3000/reservas', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body)
+  })
+    .then(res => res.json())
+    .then(data => {
+      if (data.success) {
+        alert('Reserva realizada con éxito ✅');
+        this.formularioAbiertoId = null;
+      } else {
+        alert('Error al guardar la reserva');
+      }
+    })
+    .catch(() => alert('Error de conexión con el servidor'));
+  }
 }
